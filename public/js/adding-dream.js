46fe80d73e04817e1d7ad971c0881609e963a5b9
Rachel <marcotter25@yahoo.com>
2022-10-20 16:13:58 -0700

    seperate public from private dreams

--- public/js/adding-dream.js
+++ public/js/adding-dream.js
@@ -2,15 +2,16 @@ async function addDreamHandler(event) {
     event.preventDefault();
 
     const description = document.querySelector('textarea[name="dream-body"]').value.trim();
-
+    const public = document.getElementById('public-dream').checked;
     const title = document.querySelector('#dream-title').value.trim();
 
-
+        console.log(public)
         const response = await fetch('/api/dreams',{
             method: 'POST',
             body: JSON.stringify({
                 description,
                 title,
+                public
             }),
             headers: {
                 'Content-type': 'application/json'
