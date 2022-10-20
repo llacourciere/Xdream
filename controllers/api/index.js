14c5668a492ade5c6687351e0b68aa9b6d7e4c80
LaLacou <108777796+users.noreply.github.com>
2022-10-19 20:50:06 -0700

    added to index api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,6 +4,7 @@ const userRoutes = require('./user-routes')
 const dreamRoutes = require('./dream-routes');
 const commentRoutes = require('./comment-routes');
 
+
 router.use('/users', userRoutes);
 router.use('/dreams', dreamRoutes);
 router.use('/comments', commentRoutes);
