b1dc16308b48dd66dadc519c2cf1441df966edd7
LaLacou <108777796+users.noreply.github.com>
2022-10-19 20:56:45 -0700

    hashtag password

--- public/js/login.js
+++ public/js/login.js
@@ -28,7 +28,7 @@ async function loginFormHandler(event) {
     event.preventDefault();
 
     const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('password-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
 
     if(email && password) {
         const response = await fetch('api/users/login', {
