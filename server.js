5c590838395597ba4ee73450f653ad47d34b49c5
alotofsnakes <mrfries@gmail.com>
2022-10-21 16:40:49 -0700

    date format helper added

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
