63d81c8980f0f7f64c77635cff754dcb5e4e1554
alotofsnakes <mrfries@gmail.com>
2022-10-21 17:50:21 -0700

    added comment functionality and fixed edit

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -15,7 +15,7 @@ router.post('/', withAuth, (req, res) => {
     if (req.session) {
         Comment.create({
             comment_text: req.body.comment_text,
-            post_id: req.body.post_id,
+            dream_id: req.body.dream_id,
             user_id: req.session.user_id
         })
             .then(dbCommentData => res.json(dbCommentData))
