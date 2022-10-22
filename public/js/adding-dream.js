fb24b832fd292f153f5650116fe642eca1538c4f
alotofsnakes <mrfries@gmail.com>
2022-10-22 16:56:46 -0700

    added tags functionality

--- public/js/adding-dream.js
+++ public/js/adding-dream.js
@@ -1,6 +1,25 @@
-const tag = fetch('/api/tags')
-.then((response) => response.json())
-.then((data) => console.log(data))
+let tag_id
+$('#nightmare').on('click', function(){
+    tag_id = 1
+});
+$('#lucid').on('click', function(){
+    tag_id = 2
+});
+$('#realistic').on('click', function(){
+    tag_id = 3
+});
+$('#reoccurring').on('click', function(){
+    tag_id = 4
+});
+$('#prophetic').on('click', function(){
+    tag_id = 5
+});
+$('#daydream').on('click', function(){
+    tag_id = 6
+});
+$('#erotic').on('click', function(){
+    tag_id = 7
+});
 
 async function addDreamHandler(event) {
     event.preventDefault();
@@ -8,16 +27,14 @@ async function addDreamHandler(event) {
     const description = document.querySelector('textarea[name="dream-body"]').value.trim();
     const public = document.getElementById('public-dream').checked;
     const title = document.querySelector('#dream-title').value.trim();
-
-  
-
         console.log(public)
         const response = await fetch('/api/dreams',{
             method: 'POST',
             body: JSON.stringify({
                 description,
                 title,
-                public
+                public,
+                tag_id
             }),
             headers: {
                 'Content-type': 'application/json'
@@ -29,5 +46,6 @@ async function addDreamHandler(event) {
             alert(response.statusText);
         }
     }
+
 
 document.querySelector('.new-dream-form').addEventListener('submit', addDreamHandler)
